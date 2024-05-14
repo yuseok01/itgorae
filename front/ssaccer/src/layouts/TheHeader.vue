80f3ed3b65038d40f8949668edfae41d93e9b75e
Iseo <chxiseo@naver.com>
2024-05-14 17:55:44 +0900

    0514

---
+++ front/ssaccer/src/layouts/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>
+    <a href="#">
+      <img src="assets/logo.png" alt="logo" />
+    </a>
+  </h1>
+  <nav class="main-navigation">
+    <a class="active" href="#">이적 시장</a>
+    <a href="#">리그</a>
+    <a href="#">팀 매뉴</a>
+    <a href="#">가입하기</a>
+  </nav>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
