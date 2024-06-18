f8f45f1ed2eb41907aa457c18fe2d516bd56588e
unknown <itgorae.seok@gmail.com>
2024-06-18 11:31:43 +0900

    0618

---
+++ java/Practice/src/BaekJoon_9_12919_A와B2.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon_9_12919_A와B2 {
+	private static String init;
+	private static String goal;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		init = br.readLine(); 
+		goal = br.readLine();
+		
+		
+		
+	}
+}
