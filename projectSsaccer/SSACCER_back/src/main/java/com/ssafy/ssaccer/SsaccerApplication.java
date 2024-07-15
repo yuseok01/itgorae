ab0f72c783b02f8274bc101d29f2a576efbd8a4e
김유석 <kurladbtjr@naver.com>
2024-07-15 17:49:27 +0900

    0715

--- projectSsaccer/SSACCER_back/src/main/java/com/ssafy/ssaccer/SsaccerApplication.java
+++ projectSsaccer/SSACCER_back/src/main/java/com/ssafy/ssaccer/SsaccerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SsaccerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SsaccerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SsaccerApplication.class, args);
+    }
 
 }
