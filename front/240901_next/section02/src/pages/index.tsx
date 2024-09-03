255690296c05175b02cabcbf00594dcd83744394
itgorae.seok <kurladbtjr@naver.com>
2024-09-03 18:02:36 +0900

    09034

--- front/240901_next/section02/src/pages/index.tsx
+++ front/240901_next/section02/src/pages/index.tsx
@@ -1,20 +1,40 @@
 //CSS Moudule
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import books from '@/mock/books.json'
 import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType, InferGetStaticPropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export default function Home() {
+//약속된 함수 SSR 설정 HOME을 실행시키기 전에 먼저 실행됨  
+export const getStaticProps = async() => {
+
+  //인수로 전달한 모든 비동기함수를 동시에 실행
+const [allBooks, recoBooks] = await Promise.all([
+  fetchBooks(),
+  fetchRandomBooks(), //인수 전달 
+]);
+return {  
+  props: {
+    allBooks,
+    recoBooks,
+  },
+ };
+};
+
+export default function Home({allBooks,recoBooks} : InferGetStaticPropsType<typeof getStaticProps>) {
+
   return (
     <div className={style.container}>
     <section>
       <h3>지금 추천하는 도서</h3>
-      {books.map((book)=><BookItem key={book.id}{...book}/>)}
+      {recoBooks.map((book)=><BookItem key={book.id}{...book}/>)}
       </section>
     <section>
       <h3>등록된 모든 도서</h3>
-      {books.map((book)=><BookItem key={book.id}{...book}/>)}
+      {allBooks.map((book)=><BookItem key={book.id}{...book}/>)}
     </section>
   </div>
   )
--- front/240901_next/section02/src/pages/search/index.tsx
+++ front/240901_next/section02/src/pages/search/index.tsx
@@ -3,6 +3,18 @@ import { useRouter } from "next/router";
 import { ReactNode } from "react";
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { GetServerSidePropsContext } from "next";
+
+
+export const getServerSideProps = async (context : GetServerSidePropsContext) => {
+  //브라우저로부터 받은 모든 정보 -> 쿼리받아오기
+  //console.log(context);
+  const q = context.query.q;
+  return {
+    props: {},
+  };
+}
+
 
 export default function Page() {
   // 함수 이름을 'SearchPage'로 변경
